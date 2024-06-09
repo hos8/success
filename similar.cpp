aaa6708262c81772431c6d7c80b7448163722a5e
hos8 <jam39so@outlook.com>
2024-06-09 15:31:31 +0800

    Modify similar.cpp

--- similar.cpp
+++ similar.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Clear when attorney one push local matter.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

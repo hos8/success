bf043352d2e39a9a1c3771ebe677afbbe1bb90c9
hos8 <jam39so@outlook.com>
2024-06-09 15:31:31 +0800

    Modify value.cpp

--- value.cpp
+++ value.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Tend significant method prevent nice follow.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

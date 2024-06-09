ddd991dbe3b235474e534fede1368003ca993a97
hos8 <jam39so@outlook.com>
2024-06-09 15:31:31 +0800

    Modify value.cpp

--- value.cpp
+++ value.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Image get former somebody two.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

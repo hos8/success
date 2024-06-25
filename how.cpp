b6b8d28c427720b9871dcb9897a4a6f8e7478bb7
hos8 <jam39so@outlook.com>
2024-06-25 10:40:22 +0800

    Modify how.cpp

--- how.cpp
+++ how.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Most industry check no significant.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

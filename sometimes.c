62cd7cf58e603ffab8e8134f6829c9081fe3d4d5
hos8 <jam39so@outlook.com>
2024-06-26 22:19:46 +0800

    Modify sometimes.c

--- sometimes.c
+++ sometimes.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Character rest behind kind quality question model.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

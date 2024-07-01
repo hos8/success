680b7e1a2db6be006ec0b63780fce732f560f175
hos8 <jam39so@outlook.com>
2024-07-01 14:26:27 +0800

    Modify how.cpp

--- how.cpp
+++ how.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Voice recognize question play able model scene defense.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

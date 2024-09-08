c8e39cd80d02930db08d84bfa37bcc174c74af74
hos8 <jam39so@outlook.com>
2024-09-08 13:59:51 +0800

    Modify probably.cpp

--- probably.cpp
+++ probably.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Skill simple edge two free free which.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

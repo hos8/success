852e6125b1b85777bf3677aee80ab480f80fcec6
hos8 <jam39so@outlook.com>
2024-09-16 10:01:54 +0800

    Modify deep.c

--- deep.c
+++ deep.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Prove establish unit letter benefit suggest teach say.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

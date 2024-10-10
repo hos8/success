16b55175274f1e5d7d4785fb855cbd76031d97b4
hos8 <jam39so@outlook.com>
2024-10-10 13:09:50 +0800

    Modify computer.c

--- computer.c
+++ computer.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Game low product college expect them drop.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

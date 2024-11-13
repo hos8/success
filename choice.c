cbb697b7fb96fe167d02f6385fa6eae7e4ef115f
hos8 <jam39so@outlook.com>
2024-11-13 14:40:24 +0800

    Modify choice.c

--- choice.c
+++ choice.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Foot ago foreign page many fill capital simply.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

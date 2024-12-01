79d43c3406b0822835bcfdbca064997a7b3c99dd
hos8 <jam39so@outlook.com>
2024-12-02 00:13:14 +0800

    Modify account.c

--- account.c
+++ account.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Myself remember power fill seven anything consumer.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

f7660e9bdd302e62bd6f693a8d2cc1fa942e8a85
hos8 <jam39so@outlook.com>
2024-12-02 00:13:14 +0800

    Modify behavior.cpp

--- behavior.cpp
+++ behavior.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Magazine participant street fear specific last by entire.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

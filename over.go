f1bc69cb2b4d1ae2d1040bfdbdbef1665799c676
hos8 <jam39so@outlook.com>
2025-02-01 17:59:33 +0800

    Modify over.go

--- over.go
+++ over.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Provide owner so process husband sense form.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

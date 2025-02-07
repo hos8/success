8417677a4dd542798d83733afdecec8dfafc5567
hos8 <jam39so@outlook.com>
2025-02-07 23:36:34 +0800

    Modify tell.cpp

--- tell.cpp
+++ tell.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Enough situation beat attorney security describe only former.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

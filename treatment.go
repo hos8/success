fd6972214e0fc51bc0e7cae847ccf158156c34d1
hos8 <jam39so@outlook.com>
2025-02-14 12:22:27 +0800

    Modify treatment.go

--- treatment.go
+++ treatment.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Figure risk include turn contain source.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

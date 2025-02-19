c067ff9b206d22abac6550f8c682c45de01ba729
hos8 <jam39so@outlook.com>
2025-02-19 20:58:44 +0800

    Modify at.c

--- at.c
+++ at.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Sometimes capital issue physical office citizen.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
